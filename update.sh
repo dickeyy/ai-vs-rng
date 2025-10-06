4eddab2eae9c962aef96232a0ecccfcbab6df713
Kyle Dickey <k@kyle.so>
2025-10-06 14:56:47 -0600

    update update script

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 git pull
 rm -rf ./build
 pm2 delete "cis-320 --debug"
-go build . -o build/cis-320
+go build -o build/cis-320 .
 # wait for the build to finish
 sleep 5
 pm2 start "build/cis-320 --debug"
